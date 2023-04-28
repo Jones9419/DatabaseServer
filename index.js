8b14256f72bc8fd1f7f90a0803b4b4fcca44c631
Brent Jones <michaeljones081615@gmail.com>
2023-04-27 20:09:35 -0400

    serverupdate

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express');
+const pgp = require('pg-promise')();
 const server = express();
 
 const port = 3000;
 
+
+const cn = {
+  host: 'localhost',
+  port: 5432,
+  database: 'test',
+  user: 'jones9419',
+  password: '',
+  allowExitOnIdle: true
+};
+
+const db = pgp(cn);
+
 server.get('/heartbeat', (req, res) => {
   res.send('heartbeat');
 });
+
+server.get('/students', async (req,res) => {
+  const students = await getStudents();
+  res.json({
+    students
+  })
+});
+
+
+async function getStudents() {
+  const students = await db.any('select * from "Students"', [true])
+}
 
 server.listen(port, () => {
   console.log(`Server listening on ${port}`);
